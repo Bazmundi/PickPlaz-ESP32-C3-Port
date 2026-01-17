00e8b1036e13c23fa39efd71180e6b34882d2827
Bazmundi <organicmonkeymotion@gmail.com>
2026-01-17 12:15:35 +1030

    Docs: move scaffolding off dev branch

--- ESP32Port/pickplazESP32Port/docs/source/conf.py
+++ ESP32Port/pickplazESP32Port/docs/source/conf.py
@@ -29,7 +29,6 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_css_files = ['custom.css']
 
 # -- Breathe + Exhale configuration -----------------------------------------
 
