2cb08c2df920f174e98b5249f8ad2f2306db427a
Bazmundi <organicmonkeymotion@gmail.com>
2026-01-19 14:11:22 +1030

    Docs: switch Sphinx theme to classic

--- ESP32Port/pickplazESP32Port/docs/source/conf.py
+++ ESP32Port/pickplazESP32Port/docs/source/conf.py
@@ -27,7 +27,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'groundwork'
+html_theme = 'classic'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
